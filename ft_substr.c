0566fbd7c19e0a6f1b9c21fb4dab4615df8e8765
avyrton <84188365+avyrton@users.noreply.github.com>
2021-05-15 11:52:06 +0300

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -2,24 +2,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	k;
-	size_t	l;
+	size_t	i;
 	char	*final;
-
-	k = 0;
-	l = 0;
-	final = (char *)malloc(sizeof(s) * (len + 1));
-	if (!final || !s)
-		return (NULL);
-	while (s[k])
+	
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+	final = malloc(sizeof(char))
+	if (i < star)
 	{
-		if (k >= start && l < len)
-		{
-			final[l] = s[k];
-			l++;
-		}
-		k++;
+		if (!final)
+			return (NULL);
+		*final = '\0'
+		return (final);
 	}
-	final[l] = '\0';
-	return ((char *)final);
+	else if (s)
+	{
+		return (ft_strndup(&s[start], len));
+	}
+	else
+		return (NULL);
 }
