967d5ccfde2f5bf8e77121cc9b6a04539a59bb07
avyrton <84188365+avyrton@users.noreply.github.com>
2021-05-15 11:28:19 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -2,16 +2,23 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	z_cad;
+	size_t	inic;
+	size_t	fnl;
 	char	*novacad;
 
 	if (!s1 || !set)
 		return (NULL);
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
-	z_cad = ft_strlen(s1);
-	while (z_cad && ft_strchr(set, s1[z_cad]))
-		z_cad--;
-	novacad = ft_substr((char *) s1, 0, z_cad + 1);
+	inic = 0;
+	while (s1[inic] && ft_strchr(set, si[inic]))
+		inic++;
+	fnl = ft_strlen(s1 + inic);
+	if (fnl)
+		while (s1[fnl + inic - 1] != 0 && ft_strchr(set, s1[fnl +inic - 1]) != 0)
+		fnl--;
+	novacad = malloc(sizeof(char) * fnl + 1);
+	if (!novocad)
+		return (NULL);
+	ft_memcpy(novacad, s1 + inic, fnl);
+	novacad[fnl + 1] = '\0'
 	return (novacad);
 }
